ef2fe45a1c9e31921e8be5c5ba5844447ccd4a2e
atodor5 <atodor5@yahoo.com>
2011-09-24 13:03:31 -0700

    rails start

---
+++ test_resume/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestResume::Application.config.secret_token = 'a8ca317180fb52b6fd04cdb21b1c60e7b693aa3a313a47cf6b1ec92bf0a316d173be405d3da9a9c6e39565b72b5d9952b1df29871cf18ee43932e2ca6ab6c5b7'
